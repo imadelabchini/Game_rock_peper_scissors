988f285cedefe759c27346aa03a6f70064780b38
imade <imade.labchini@uit.ac.ma>
2024-11-25 10:25:31 -0800

    Update Game_rock_peper_scissors_v2.py
    Game rock_peper_scissores

--- Game_rock_peper_scissors.py
+++ Game_rock_peper_scissors.py
@@ -17,11 +17,11 @@ print("Welcome to the Rock, Peper, Scissors game:")
 confirm = input("Press Enter to continue or type (Help) for the rules: ").lower()
 if confirm == "help" :
     print("""
-******** Rules *********
-1) You choose and the computer chooses
-2) Rock smashes Scissors -> Rock wins
-3) Scissors cut Peper ->Scissors win
-4) Peper covers Rock -> Peper wins                  
+          ******** Rules *********
+          1) You choose and the computer chooses
+          2) Rock smashes Scissors -> Rock wins
+          3) Scissors cut Peper ->Scissors win
+          4) Peper covers Rock -> Peper wins                  
 """)
     
 user_choice = input("Enter your choice (rock, peper, scissors): ").lower()
@@ -34,31 +34,36 @@ if user_choice in ['rock','peper','scissors'] :
         print(f"\nYou choice:\n{peper_ascki}")
     else :
         print(f"\nYou choice:\n{scissors_ascki}")
+        
+    # computer choice
+    computer_choice = random.choice(['rock','peper','scissors'])
+    if computer_choice == 'rock' :
+        print(f"\nComputer choice:\n{rock_ascki}")
+    elif computer_choice == 'peper' :
+        print(f"\nComputer choice:\n{peper_ascki}")
+    else :
+        print(f"\nComputer:\n{scissors_ascki}")
+
+        # Detrnine the winner
+    if(
+        user_choice == "rock"  and computer_choice == "scissors"  
+        or
+        user_choice == "scissors"  and computer_choice == "peper"
+        or     
+        user_choice == "peper"  and computer_choice == "rock"    
+    ) :
+        print(f"You win! {user_choice} beats {computer_choice}")
+
+    elif computer_choice == user_choice :
+        print("It's a tie!")
+    else :
+        print(f"You lose! {user_choice} beats {computer_choice}")
 else :
      print("invalid choice. Please run the progrm again and choose rock, pepre, or scisors")  
 
-# computer choice
-computer_choice = random.choice(['rock','peper','scissors'])
-if computer_choice == 'rock' :
-        print(f"\nComputer choice:\n{rock_ascki}")
-elif computer_choice == 'peper' :
-        print(f"\nComputer choice:\n{peper_ascki}")
-else :
-        print(f"\nComputer:\n{scissors_ascki}")
+
 
      
-# Detrnine the winner
-if(
-user_choice == "rock"  and computer_choice == "scissors"  
-or
-user_choice == "scissors"  and computer_choice == "peper"
-or     
-user_choice == "peper"  and computer_choice == "rock"    
-) :
-    print(f"You win! {user_choice} beats {computer_choice}")
 
-elif computer_choice == user_choice :
-    print("It's a tie!")
-else :
-    print(f"You lose! {user_choice} beats {computer_choice}")
+
 
